608a1853a944855d8381c5d43f247d7e14fed9ee
Matthieu Veillon <matthieu.veillon@gmail.com>
2020-12-19 14:14:50 +0100

    add logging to sequelize

--- api/src/config/db.config.js
+++ api/src/config/db.config.js
@@ -5,6 +5,8 @@ module.exports = {
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST || "postgres", // from docker compose service name
   port: process.env.DATABASE_PORT,
+  logging: true,
+
   define: {
     // disable the modification of table names; By default, sequelize will automatically
     // transform all passed model names (first parameter of define) into plural.
